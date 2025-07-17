3e0cbcb733bc208036f836cf65f128fa5b88b569
Yuval Nahshony <200560745@student.georgianc.on.ca>
2025-07-16 20:43:03 -0400

    updated json file to accurately reflect the images, and host my own json file that is in the repo

--- js/json.js
+++ js/json.js
@@ -6,7 +6,7 @@ async function populate(){
 // Introducing JavaScript Object Notation (JSON): https://json.org/
 // STEP 4a: Create i-scream.json file with companyName, headOffice, established, active, topFlavors(name, calories, type, ingredients, image) */
 // STEP 4b: Store the URL of a JSON file in a variable */
-let url = "https://priyansht.github.io/25S-JavaScript-01-Week10/js/i-scream.json";
+let url = "https://yuvalnahshony895.github.io/lab3/js/i-scream.json";
 // STEP 5: Use the new URL to create a new request object
 let request = new Request(url);
 console.log(request);
